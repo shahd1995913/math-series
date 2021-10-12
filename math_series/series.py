dccac6586c3a8c9d4f20fdfbbd1e87bf00b6b898
shahd1995913 <shahdalkhatib95@gmail.com>
2021-10-12 15:16:55 +0300

    doc string  and fix the bugs

--- math_series/series.py
+++ math_series/series.py
@@ -1,7 +1,12 @@
 
-
-
 def  Lucas(input) :
+    """ 
+Lucas function that take input and send it to anther function 
+called  sum_series.
+Lucas numbers  defined as the sum of its two.
+But here the first two terms are 2 and 1.
+ 
+"""
 
     # if input ==  0 :
 
@@ -18,7 +23,17 @@ def  Lucas(input) :
     return  sum_series(input,2,1)
 
 
-def fibonacci(input) :
+def fibonacci(input) : 
+    """ 
+A Fibonacci sequence is integer sequence of 0, 1, 1, 2, 3, 5, 8....
+fibonacci function that take input and send it to anther function 
+called  sum_series
+
+
+"""
+
+
+
 
 
 #   if input == 0:
@@ -42,6 +57,10 @@ def fibonacci(input) :
    
 
 def sum_series(input,input1=0,input2=1):
+    """ 
+ sequence up to n-th term
+by function called a sum_series that takes a 3 parameter input from the 
+Fibonacci and Lucas functionsn """
 
     if input == 0:
 
